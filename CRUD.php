847e0b97cf570fec7edce2c1324410fa152d5a57
ziad <z.walid59@gmail.com>
2018-11-24 00:05:17 +0300

    mini project 1

--- CRUD.php
+++ CRUD.php
@@ -1,114 +1,108 @@
 <?php
 $method = $_SERVER['REQUEST_METHOD'];
-function db_connect()
-{
+
+function db_connect() {
   $servername = "localhost";
   $username = "root";
-  $password = "machine1";
+  $password = "";
   $dbname = "contacts";
   /// Create connection
   $conn = new mysqli($servername, $username, $password,$dbname);
   if ($conn->connect_error) {
-  	die("Connection failed: " . $conn->connect_error);
-} return $conn;
+    die("Connection failed: " . $conn->connect_error);
+  } return $conn;
 }//db_connect close
-function RetriveContact($id)
-{ $conn = db_connect();
- //echo "get contact func";
-//$sql = "SELECT id, first_name, last_name FROM MyGuests";
+function RetriveContact($id) {
+  $conn = db_connect();
+  //echo "get contact func";
+  //$sql = "SELECT id, first_name, last _name FROM MyGuests";
   $sql = "select * from `contact`".($id?" WHERE id=$id":'');
   $con_results = mysqli_query($conn, $sql);
   $data = array();//this array to save data from db
   $result = mysqli_fetch_assoc($con_results);
- $data[] = $result;
+  $data[] = $result;
+  $phone = array();
 
- if(mysqli_num_rows($con_results) > 0){
-
-   $sql = "SELECT phone_number FROM phone_numbers WHERE contact_id ='$id' ";
-   $con_results = mysqli_query($conn, $sql);
-   $result = mysqli_fetch_assoc($con_results);
-   $phone[] = $result;
-
- }
- print_r(json_encode($data));
- print_r(json_encode($phone));
- die();
+  if(mysqli_num_rows($con_results) > 0){
+    $sql = "SELECT phone_number FROM phone_numbers WHERE contact_id ='$id' ";
+    $con_results = mysqli_query($conn, $sql);
+    $result = mysqli_fetch_assoc($con_results);
+    $phone[] = $result;
+  }
+  $response = ['Data' => $data, 'Phone' => $phone];
+  print_r(json_encode($response));
+  //print_r(json_encode($data));
+  //print_r(json_encode($phone));
+  die();
 }
-
-function InsertData()
-{
+function InsertData() {
   if (file_get_contents('php://input',true)){
-  $json_requst =file_get_contents('php://input',true);
-  $data = json_decode($json_requst, TRUE);
- //print_r($data);
-  $name = $data['first_name'];
-  $surname = $data['last_name'];
-  $phone = $data['phone_number'];
-   }
+    $json_requst =file_get_contents('php://input',true);
+    $data = json_decode($json_requst, TRUE);
+    //print_r($data);
+    $name = $data['first_name'];
+    $surname = $data['last_name'];
+    $phone = $data['phone_number'];
+  }
   $conn = db_connect();
   $name = mysqli_real_escape_string($conn,$name);
   $surname = mysqli_real_escape_string($conn,$surname);
   $phone = mysqli_real_escape_string($conn,$phone);
-      $sql = "INSERT INTO contact"." (first_name,last_name)"." VALUES ('".  $name."' , '".$surname."');";
-      $results = mysqli_query($conn, $sql);
-      if($results){
-        $contactID = mysqli_insert_id($conn);
+  $sql = "INSERT INTO contact"." (first_name,last_name)"." VALUES ('".  $name."' , '".$surname."');";
+  $results = mysqli_query($conn, $sql);
+  if($results){
+    $contactID = mysqli_insert_id($conn);
 
-        $sql = "INSERT INTO phone_numbers" ." (phone_title,phone_number, default_num ,contact_id)"
-        ." VALUES ("."'HOME'".",".$phone.","."1".",".$contactID.");";
-        $results = mysqli_query($conn, $sql);
-        echo "data inserted in tables";
-        if(!$results ){
-          die("SQL error " . mysqli_error($conn));
-        }
-      } else{
-        die("SQL error " . mysqli_error($conn));
-      }
+    $sql = "INSERT INTO phone_numbers" ." (phone_title,phone_number, default_num ,contact_id)"
+    ." VALUES ("."'HOME'".",".$phone.","."1".",".$contactID.");";
+    $results = mysqli_query($conn, $sql);
+    echo "data inserted in tables";
+    if(!$results ){
+      die("SQL error " . mysqli_error($conn));
+    }
+  } else{
+    die("SQL error " . mysqli_error($conn));
+  }
 }
-
-
-function UpdateData()
-{
-   if (file_get_contents('php://input',true)){
-   $json_requst =file_get_contents('php://input',true);
-   $data = json_decode($json_requst, TRUE);
-  //print_r($data);
-   $id = $data["id"];//print_r($id);
-   $name = $data['first_name'];
-   $surname = $data['last_name'];
-   $phone = $data['phone_number'];
- }
-$conn = db_connect();
- $sql = " UPDATE contact SET first_name ='$name' ,  last_name ='$surname'  WHERE id= '$id' " ;
- $result = mysqli_query($conn,$sql);
-
- if ($result) {
-   echo " data is updated ";
-   echo "<br/>";
- }else {
-   die("data failed".mysqli_error($conn));
-   echo "<br/>";
- }
-
- $sql = " UPDATE phone_numbers SET phone_number ='$phone' WHERE contact_id= '$id' " ;
- $result = mysqli_query($conn,$sql);
-
- if ($result) {
-   echo "phone updated <br>";
- }else {
-   die("data failed".mysqli_error($conn));
-   echo "<br/>";
- }
-}// update func close
-
- function DeleteData()
-{
+function UpdateData() {
   if (file_get_contents('php://input',true)){
-  $json_requst =file_get_contents('php://input',true);
-  $data = json_decode($json_requst, TRUE);
- //var_dump($data);
-  $id = $data["id"];//var_dump($id);
-}
+    $json_requst =file_get_contents('php://input',true);
+    $data = json_decode($json_requst, TRUE);
+    //print_r($data);
+    $id = $data["id"];//print_r($id);
+    $name = $data['first_name'];
+    $surname = $data['last_name'];
+    $phone = $data['phone_number'];
+  }
+  $conn = db_connect();
+  $sql = " UPDATE contact SET first_name ='$name' ,  last_name ='$surname'  WHERE id= '$id' " ;
+  $result = mysqli_query($conn,$sql);
+
+  if ($result) {
+    echo " data is updated ";
+    echo "<br/>";
+  }else {
+    die("data failed".mysqli_error($conn));
+    echo "<br/>";
+  }
+
+  $sql = " UPDATE phone_numbers SET phone_number ='$phone' WHERE contact_id= '$id' " ;
+  $result = mysqli_query($conn,$sql);
+
+  if ($result) {
+    echo "phone updated <br>";
+  }else {
+    die("data failed".mysqli_error($conn));
+    echo "<br/>";
+  }
+}// update func close
+function DeleteData() {
+  if (file_get_contents('php://input',true)){
+    $json_requst =file_get_contents('php://input',true);
+    $data = json_decode($json_requst, TRUE);
+    //var_dump($data);
+    $id = $data["id"];//var_dump($id);
+  }
   $conn = db_connect();
   $id = mysqli_real_escape_string($conn,$id);//var_dump($id);
   $sql = "DELETE FROM contact WHERE id='$id'";
@@ -124,20 +118,22 @@ $conn = db_connect();
 }//delete function close
 
 switch ($method) {
-case 'GET': //retrive
-$id = $_GET['id'];
-//print_r($id);
-//function to retrive contact
-RetriveContact($id);
-break;
-case 'PUT'://update
-UpdateData();
-break;
-case 'POST'://insert into table
-InsertData();
-break;
-case 'DELETE'://delete data from table
-DeleteData();
+  case 'GET': //retrive
+  if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    RetriveContact($id);
+  }else echo "no id was sent";
+  //print_r($id);
+  //function to retrive contact
+  break;
+  case 'PUT'://update
+  UpdateData();
+  break;
+  case 'POST'://insert into table
+  InsertData();
+  break;
+  case 'DELETE'://delete data from table
+  DeleteData();
 
 }
-  ?>
+?>
--- login.php
+++ login.php
@@ -1,48 +1,47 @@
- <?php
+<?php
 /// require_once("CRUD_functions.php");
- if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-echo "post method<br>";
-$data = json_decode(file_get_contents('php://input'), true);
-//print_r($data);echo "<br>";
-$_POST['username'] = $data['username'];
-$_POST['password'] = $data['password'];
-//print_r( $_POST['password']);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  echo "post method<br>";
+  $data = json_decode(file_get_contents('php://input'), true);
+  //print_r($data);echo "<br>";
+  $_POST['username'] = $data['username'];
+  $_POST['password'] = $data['password'];
+  //print_r( $_POST['password']);
 
-/// databasr cretantials
-$servername = "localhost";
-$username = "root";
-$password = "machine1";
-$dbname = "contacts";
-/// Create connection
-$conn = new mysqli($servername, $username, $password,$dbname);
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-}
-          echo "Connected successfully to database <br/> ";
-///sql injection
-$userid = mysqli_real_escape_string($conn,$_POST['username']);
-$pass = mysqli_real_escape_string($conn,$_POST['password']);
+  /// databasr cretantials
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "contacts";
+  /// Create connection
+  $conn = new mysqli($servername, $username, $password,$dbname);
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully to database <br/> ";
+  ///sql injection
+  $userid = mysqli_real_escape_string($conn,$_POST['username']);
+  $pass = mysqli_real_escape_string($conn,$_POST['password']);
 
-$sql = "SELECT * FROM login WHERE username  = '$userid'and password = '$pass'  ";
-//print_r($sql);
+  $sql = "SELECT * FROM login WHERE username  = '$userid'and password = '$pass'  ";
+  //print_r($sql);
 
-$con_results = mysqli_query($conn, $sql);
+  $con_results = mysqli_query($conn, $sql);
 
-$usersdb = array();//this array to save data from db
-$i =0;
-while($row = mysqli_fetch_assoc($con_results)) {
-  $usersdb[]=$row;$i =$i+1;
-        print_r($usersdb[0]['username']);
+  $usersdb = array();//this array to save data from db
+  $i =0;
+  while($row = mysqli_fetch_assoc($con_results)) {
+    $usersdb[]=$row;
+    $i =$i+1;
+    print_r($usersdb[0]['username']);
+    echo " <br>“status code”:200 <br/>";
+    echo "user found ";
+    echo "<br>you have logged in<br>";
+  }
 
-        echo " <br>“status code”:200 <br/>";
-        echo "user found ";
-        echo "<br>you have logged in<br>";
-}
-
-    if($i== 0){
-	echo " “error_code”:101 <br/>";
-	echo "user not found ";
-   }
- //while
+  if($i== 0){
+    echo " “error_code”:101 <br/>";
+    echo "user not found ";
+  }//while
 }//if method post
- ?>
+?>
